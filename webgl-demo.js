a969a186ee1a4699963b5d69fe694aebb7a6646e
Walter Reis <9714275+Xinchro@users.noreply.github.com>
2022-12-14 13:33:17 +0000

    rotating cube

--- webgl-demo.js
+++ webgl-demo.js
@@ -1,8 +1,12 @@
 import { initBuffers } from "./init-buffers.js";
 import { drawScene } from "./draw-scene.js";
 
+let squareRotation = 0.0;
+let deltaTime = 0;
+
 
 main();
+
 
 //
 // start here
@@ -72,8 +76,20 @@ function main() {
   // objects we'll be drawing.
   const buffers = initBuffers(gl);
 
-  // Draw the scene
-  drawScene(gl, programInfo, buffers);
+  let then = 0;
+
+  // Draw the scene repeatedly
+  function render(now) {
+    now *= 0.001; // convert to seconds
+    deltaTime = now - then;
+    then = now;
+
+    drawScene(gl, programInfo, buffers, squareRotation);
+    squareRotation += deltaTime;
+
+    requestAnimationFrame(render);
+  }
+requestAnimationFrame(render);
 
 }
 
